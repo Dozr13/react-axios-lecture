39030068569cd69095e37d163f0806e747886504
Wade Pate <wadejp8@gmail.com>
2021-01-26 12:22:53 -0700

    Axios practice pokemon cards

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+    // ! Declared the card props below
 const Card = (props) => {
     return <div className="card">
-        
+        <img alt={props.name} src={props.imageUrl} />
+        <span>{props.name}</span>
     </div>
 }
 
